b2d4424128aff46665e1cd64b77f7d5990f07d1b
Verkholomova Karina <verholomova_karina@ukr.net>
2022-07-25 17:04:00 +0200

    Bottom Sheet updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,13 @@ import 'dart:math' as math;
 
 import 'package:map_pro/models/volunteer_centers.dart';
 
-
 final LatLng _center = const LatLng(54.4641, 17.0287);
-void main(){
+
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,21 +22,50 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   GoogleMapController? mapController; //contrller for Google map
   Set<Marker> markers = Set(); //markers for google map
-  //
-  // LatLng startLocation = LatLng(27.6602292, 85.308027);
-  // LatLng endLocation = LatLng(27.6599592, 85.3102498);
-  // LatLng carLocation = LatLng(27.659470, 85.3077363);
 
+  Column _buildBottonNavigationMethod(name, address, imageURL, workHours) {
+    return Column(
+      // mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Image.asset(
+          imageURL,
+          // width: 50.0,
+          // height: 200.0,
+          // fit: BoxFit.cover,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: Icon(Icons.maps_home_work_outlined),
+            title: Text(name),
+            subtitle: Text(address),
+          ),
+        ),
+        Padding(
+          padding:  EdgeInsets.only(bottom: 20.0),
+          child: Text("Work hours: $workHours"),
+        ),
+        SizedBox(
+          width: double.maxFinite,
+          child: MaterialButton(
+            color: Colors.greenAccent,
+              onPressed: (){
 
+          },
+              child: Text("Directions", style: TextStyle(fontSize: 14),)),
+        )
+
+      ],
+    );
+  }
 
   @override
   void initState() {
@@ -49,63 +78,56 @@ class _HomeState extends State<Home> {
       ImageConfiguration(),
       "assets/images/bike.png",
     );
-    Column _buildBottonNavigationMethod(name, address) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.maps_home_work_outlined),
-            title: Text(name),
-            subtitle: Text(address),
 
-          )
-        ],
-      );
-    }
+    markers.add(Marker(
+        //add start location marker
+        markerId: MarkerId(LatLng(54.468683, 17.028140).toString()),
+        position: LatLng(54.468683, 17.028140),
+        //position of marker
+        infoWindow: InfoWindow(
+          //popup info
+          title: "Regionalne Centrum Wolontariatu",
+          snippet: "aleja Henryka Sienkiewicza 6, 76-200 Słupsk",
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+        onTap: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+              context: context,
+              builder: (builder) {
+                return Container(
+                  child: _buildBottonNavigationMethod(
+                      "Regionalne Centrum Wolontariatu",
+                      "aleja Henryka Sienkiewicza 6, 76-200 Słupsk", "images/1.jpg", "9:00 - 15:00"),
+                );
+              });
+        } //Icon for Marker
+        ));
 
-    markers.add(
-        Marker( //add start location marker
-          markerId: MarkerId(LatLng(54.468683, 17.028140).toString()),
-          position: LatLng(54.468683, 17.028140), //position of marker
-          infoWindow: InfoWindow( //popup info
-            title: "Regionalne Centrum Wolontariatu",
-            snippet: "aleja Henryka Sienkiewicza 6, 76-200 Słupsk",
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-            onTap: (){
-              showModalBottomSheet(
-                  context: context,
-                  builder: (builder) {
-                    return Container(
-                      child: _buildBottonNavigationMethod("Regionalne Centrum Wolontariatu", "aleja Henryka Sienkiewicza 6, 76-200 Słupsk"),
-                    );});
-            }//Icon for Marker
-        )
-    );
-
-    markers.add(
-        Marker( //add start location marker
-          markerId: MarkerId(LatLng(54.452438, 17.041785).toString()),
-          position: LatLng(54.452438, 17.041785), //position of marker
-          // rotation:-45,
-          infoWindow: InfoWindow( //popup info
-            title: "Pomeranian Academy in Slupsk",
-            snippet: "Krzysztofa Arciszewskiego, 76-200 Słupsk",
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-          onTap: (){
-            showModalBottomSheet(
-                context: context,
-                builder: (builder) {
-                  return Container(
-                    child: _buildBottonNavigationMethod("Pomeranian Academy in Slupsk", "Krzysztofa Arciszewskiego, 76-200 Słupsk"),
-                  );});
-          }
-        )
-    );
-
-
-
+    markers.add(Marker(
+        //add start location marker
+        markerId: MarkerId(LatLng(54.452438, 17.041785).toString()),
+        position: LatLng(54.452438, 17.041785),
+        //position of marker
+        // rotation:-45,
+        infoWindow: InfoWindow(
+          //popup info
+          title: "Pomeranian Academy in Slupsk",
+          snippet: "Krzysztofa Arciszewskiego, 76-200 Słupsk",
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+        onTap: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+              context: context,
+              builder: (builder) {
+                return Container(
+                  child: _buildBottonNavigationMethod(
+                      "Pomeranian Academy in Slupsk",
+                      "Krzysztofa Arciszewskiego, 76-200 Słupsk", "images/2.jpg", "9:00 - 15:00"),
+                );
+              });
+        }));
 
     // String imgurl = "https://www.fluttercampus.com/img/car.png";
     // Uint8List bytes = (await NetworkAssetBundle(Uri.parse(imgurl))
@@ -113,77 +135,89 @@ class _HomeState extends State<Home> {
     //     .buffer
     //     .asUint8List();
 
-    markers.add(
-        Marker( //add start location marker
-          markerId: MarkerId(LatLng(54.451206, 17.023427).toString()),
-          position: LatLng(54.451206, 17.023427), //position of marker
-          infoWindow: InfoWindow( //popup info
-            title: "Municipal Family Assistance Center",
-            snippet: "Słoneczna 15D, 76-200 Słupsk",
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-            onTap: (){
-              showModalBottomSheet(
-                  context: context,
-                  builder: (builder) {
-                    return Container(
-                      child: _buildBottonNavigationMethod("Municipal Family Assistance Center", "Słoneczna 15D, 76-200 Słupsk"),
-                    );});
-            }//Icon for Marker
-        )
-    );
-    markers.add(
-        Marker( //add start location marker
-          markerId: MarkerId(LatLng(54.458005, 17.028482).toString()),
-          position: LatLng(54.458005, 17.028482), //position of marker
-          infoWindow: InfoWindow( //popup info
-            title: "Zespół Szkół Technicznych",
-            snippet: "Karola Szymanowskiego 5, 76-200 Słupsk",
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-            onTap: (){
-              showModalBottomSheet(
-                  context: context,
-                  builder: (builder) {
-                    return Container(
-                      child: _buildBottonNavigationMethod("Zespół Szkół Technicznych", "Karola Szymanowskiego 5, 76-200 Słupsk"),
-                    );});
-            }//Icon for Marker
-        )
-    );
+    markers.add(Marker(
+        //add start location marker
+        markerId: MarkerId(LatLng(54.451206, 17.023427).toString()),
+        position: LatLng(54.451206, 17.023427),
+        //position of marker
+        infoWindow: InfoWindow(
+          //popup info
+          title: "Municipal Family Assistance Center",
+          snippet: "Słoneczna 15D, 76-200 Słupsk",
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+        onTap: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+              context: context,
+              builder: (builder) {
+                return Container(
+
+                  child: _buildBottonNavigationMethod(
+                      "Municipal Family Assistance Center",
+                      "Słoneczna 15D, 76-200 Słupsk", "images/3.jpg", "9:00 - 15:00"),
+                );
+              });
+        } //Icon for Marker
+        ));
+    markers.add(Marker(
+        //add start location marker
+        markerId: MarkerId(LatLng(54.458005, 17.028482).toString()),
+        position: LatLng(54.458005, 17.028482),
+        //position of marker
+        infoWindow: InfoWindow(
+          //popup info
+          title: "Zespół Szkół Technicznych",
+          snippet: "Karola Szymanowskiego 5, 76-200 Słupsk",
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+        onTap: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (builder) {
+                return Container(
+                  child: _buildBottonNavigationMethod(
+                      "Zespół Szkół Technicznych",
+                      "Karola Szymanowskiego 5, 76-200 Słupsk", "images/4.jpg", "9:00 - 15:00"),
+                );
+              });
+        } //Icon for Marker
+        ));
 
     setState(() {
       //refresh UI
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text("Image Marker on Google Map"),
           backgroundColor: Colors.deepPurpleAccent,
         ),
-        body: GoogleMap( //Map widget from google_maps_flutter package
-          zoomGesturesEnabled: true, //enable Zoom in, out on map
-          initialCameraPosition: CameraPosition( //innital position in map
+        body: GoogleMap(
+          //Map widget from google_maps_flutter package
+          zoomGesturesEnabled: true,
+          //enable Zoom in, out on map
+          initialCameraPosition: CameraPosition(
+            //innital position in map
             target: _center, //initial position
             zoom: 14.0, //initial zoom level
           ),
-          markers: markers, //markers to show on map
-          mapType: MapType.normal, //map type
-          onMapCreated: (controller) { //method called when map is created
+          markers: markers,
+          //markers to show on map
+          mapType: MapType.normal,
+          //map type
+          onMapCreated: (controller) {
+            //method called when map is created
             setState(() {
               mapController = controller;
             });
           },
-        )
-    );
+        ));
   }
 }
-
 
 // import 'package:flutter/material.dart';
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
