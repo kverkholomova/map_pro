47fadc30f60f87f5efc37ba7ee0919f65895a4f4
Verkholomova Karina <verholomova_karina@ukr.net>
2022-08-02 08:51:56 +0200

    Reformated code and created new files

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:map_pro/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
